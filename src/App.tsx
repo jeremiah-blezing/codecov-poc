00496b39661f440265405fe119374289e64a3802
Jeremiah Blessing <jerry@coverself.com>
2025-07-25 11:40:54 +0530

    feature calendar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import Calendar from "react-calendar";
+
 export const App = () => {
   return (
     <div>
       <h1>App</h1>
+      <Calendar
+        onChange={(change) => {
+          console.log(change);
+        }}
+      />
     </div>
   );
 };
