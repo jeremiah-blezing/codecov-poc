44dedc0e7b16f0423cd0423d46b785c93d7b6001
Jeremiah Blessing <jerry@coverself.com>
2025-07-25 11:32:46 +0530

    Changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { codecovVitePlugin } from "@codecov/vite-plugin";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    codecovVitePlugin({
+      enableBundleAnalysis: true,
+      bundleName: "next-amazon",
+      uploadToken: process.env.CODECOV_TOKEN,
+    }),
+  ],
+});
